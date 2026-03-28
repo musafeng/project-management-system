01083da02102b7c3e9184b425ba988ff50ee6da5
musafeng <msa395482@gmail.com>
2026-03-28 13:46:59 +0800

    fix: 移除不支持的 serverActionsBodySizeLimit 配置

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    // 允许 Server Action / Route Handler 接收最大 100MB 的请求体
-    serverActionsBodySizeLimit: '100mb',
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
